e6de3c323bd216dd8567fe5749708cce1b756150
david87chdz <david87chdz@gmail.com>
2024-10-04 10:25:45 +0200

    Starting course about Java

---
+++ src/SegundoPrograma.java
@@ -0,0 +1,12 @@
+public class SegundoPrograma {
+    public static void main(String[] args) {
+        int variable1 = 5;
+        int variable2 = 10;
+
+        if(variable1 > variable2) {
+            System.out.println("La variable 1 es mayor que la variable 2");
+        } else {
+            System.out.println("La variable 2 es mayor que la variable 1");
+        }
+    }
+}
